1811709df2fb5b6219f5a17602699b5071a5cb12
newyorknomad <newyorknomadllc@gmail.com>
2023-04-11 09:45:32 -0400

    Added arrays to store message parts

---
+++ script.js
@@ -0,0 +1,3 @@
+const partA = ['You shall ', 'He shall ', 'We shall ', 'She shall '];
+const partB = ['die ', 'live ', 'laugh ', 'love'];
+const partC = ['horribly!', 'forever!', 'never!', 'alone.'];

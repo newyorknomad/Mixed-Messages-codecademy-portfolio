42f294807704e33f42a0add68c5a20832e3e0205
newyorknomad <newyorknomadllc@gmail.com>
2023-04-11 10:02:39 -0400

    Added random generator and console log to print

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 const partA = ['You shall ', 'He shall ', 'We shall ', 'She shall '];
-const partB = ['die ', 'live ', 'laugh ', 'love'];
+const partB = ['die ', 'live ', 'laugh ', 'love '];
 const partC = ['horribly!', 'forever!', 'never!', 'alone.'];
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  };
+console.log(partA[getRandomInt(3)] + partB[getRandomInt(3)] + partC[getRandomInt(3)]);
